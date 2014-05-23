3f62d0f58aedb82634c8434221afde11c6d0ed6b
kfl62 <kfl62g@gmail.com>
2014-05-23 11:34:16 +0300

    models: `Clns::Freight`, add field, handle sorting ... #12

--- system/models/clns/freight.rb
+++ system/models/clns/freight.rb
@@ -3,12 +3,15 @@ module Clns
   class Freight < Trst::Freight
 
     field       :tva,      type: Float,     default: 0.24
+    field       :csn,      type: Hash
 
     has_many    :ins,      class_name: "Clns::FreightIn",       inverse_of: :freight
     has_many    :outs,     class_name: "Clns::FreightOut",      inverse_of: :freight
     has_many    :stks,     class_name: "Clns::FreightStock",    inverse_of: :freight
 
+    after_create :handle_csn
     after_update :handle_id_stats
+    after_update :order_csn
 
     class << self
       # @todo
@@ -44,106 +47,8 @@ module Clns
         ids = all.pluck(:id)
         Clns::FreightOut.where(:freight_id.in => ids)
       end
-      # # @todo
-      # def stats_pos(*args)
-      #   opts = args.last.is_a?(Hash) ? {}.merge!(args.pop) : {}
-      #   asc(:id_stats).each_with_object([]) do |f,a|
-      #     a << [f.id,f.name,*f.stats_sum(*args,opts)]
-      #   end
-      # end
-      # # @todo
-      # def stats_pos_with_pu(*args)
-      #   opts = args.last.is_a?(Hash) ? {}.merge!(args.pop) : {}
-      #   y,m,d = *args; today = Date.today
-      #   y,m,d = today.year, today.month, today.day unless ( y || m || d)
-      #   asc(:id_stats).each_with_object([]) do |f,a|
-      #     keys = (f.ins.monthly(y,m).keys + f.outs.monthly(y,m).keys + f.stks.monthly(y,m).where(:qu.ne => 0).keys + f.stks_now.where(:qu.ne => 0).keys).uniq.sort
-      #     keys.each do |key|
-      #       sum = *f.stats_sum(*args,opts.merge(key: key))
-      #       chk = (f.stks_now.by_key(key).sum(:qu) || 0.0).round(2)
-      #       m == today.month ? sum.push(chk) : sum.push(sum.last)
-      #       a << [f.id,f.name,key,*sum] unless sum.sum == 0
-      #     end
-      #   end
-      # end
-      # # @todo
-      # def stats_all(*args)
-      #   opts = args.last.is_a?(Hash) ? {all: true}.merge!(args.pop) : {all: true}
-      #   retval, sum_tot, part, fname = [], [0, 0, 0, 0], [], ''
-      #   units = Clns::PartnerFirm.unit_ids
-      #   keys(false).each_with_index do |ids,i|
-      #     part[i] = []
-      #     units.each do |u|
-      #       f = find_by(unit_id: u, id_stats: ids)
-      #       if f
-      #         sum_pos = f.stats_sum(*args,opts)
-      #         part[i] << sum_pos.pop
-      #         sum_tot = sum_tot.zip(sum_pos).map{|x| x.inject(:+)}
-      #         fname = f.name
-      #       else
-      #         part[i] << 0
-      #       end
-      #     end
-      #     retval << [fname,sum_tot,part[i],(sum_tot.last - part[i].sum).round(2)].flatten
-      #     sum_tot, fname = [0, 0, 0, 0], ''
-      #   end
-      #   retval
-      # end
-      # # @todo
-      # def stats_one(*args)
-      #   opts = args.last.is_a?(Hash) ? {}.merge!(args.pop) : {}
-      #   i,y,m = *args; today = Date.today; retval, sum_tot = [], [0,0]
-      #   f   = find(i)
-      #   y ||= today.year; m ||= today.month
-      #   days_in_month = (Date.new(y, 12, 31) << (12 - m)).day
-      #   final = f.stks.sum_stks(y,m,opts)
-      #   (1..days_in_month).each do |i|
-      #     f_ins   = f.ins.sum_ins(y,m,i,opts)
-      #     f_out   = f.outs.sum_outs(y,m,i,opts)
-      #     sum_tot = sum_tot.zip([f_ins,f_out]).map{|x| x.inject(:+)}
-      #     final   = (final + f_ins - f_out).round(2)
-      #     retval << [Date.new(y,m,i).to_s, f_ins, f_out, final] unless (f_ins == 0 && f_out == 0)
-      #   end
-      #   retval << ['TOTAL',sum_tot,final].flatten
-      # end
     end # Class methods
 
-    # # @todo
-    # def stats_sum(*args)
-    #   opts = args.last.is_a?(Hash) ? {}.merge!(args.pop) : {}
-    #   key = opts[:key] || id_stats
-    #   if opts[:all]
-    #     s = stks.by_key(key).sum_stks(*args,opts)
-    #     i = ins.by_key(key).sum_ins(*args,opts)
-    #     o = outs.by_key(key).sum_outs(*args,opts)
-    #     i_nin = ins.by_key(key).nonin.sum_ins(*args,opts)
-    #     o_nin = outs.by_key(key).nonin.sum_outs(*args,opts)
-    #     [s,i_nin,o_nin,(s + i_nin - o_nin).round(2), (s + i - o).round(2)]
-    #   else
-    #     s = stks.by_key(key).sum_stks(*args,opts)
-    #     i = ins.by_key(key).sum_ins(*args,opts)
-    #     o = outs.by_key(key).sum_outs(*args,opts)
-    #     [s,i,o,(s + i - o).round(2)]
-    #   end
-    # end
-    # # @todo
-    # def stats_one(*args)
-    #   args = args.unshift(id)
-    #   self.class.stats_one(*args)
-    # end
-    # # @todo
-    # def stks_now
-    #   stks.where(doc_stk_id: unit.stock_now.id)
-    # end
-    # # @todo
-    # def stock_by_key(key)
-    #   stks_now.by_key(key).sum(:qu) || 0
-    # end
-    # # @todo
-    # def unit
-    #   Clns::PartnerFirm.unit_by_unit_id(unit_id) rescue nil
-    # end
-    # @todo
     def key(p)
       "#{id_stats}_#{"%.4f" % p}"
     end
@@ -159,11 +64,23 @@ module Clns
 
     protected
     # @todo
+    def handle_csn
+      set :csn, {dflt: name}
+    end
+    # @todo
     def handle_id_stats
       if id_stats_changed?
         [ins,outs,stks].each do |m|
           m.where(id_stats: id_stats_was).update_all(id_stats: id_stats)
         end
+      end
+    end
+    # @todo
+    def order_csn
+      if csn.count > 1
+        h_def = {dflt: csn.delete('dflt')}
+        h_all = Hash[csn.map{|o| o.unshift(Clns::PartnerFirm.find(o[0]).name[0])}.sort.map{|o| o.shift;o}]
+        set :csn, h_def.merge(h_all)
       end
     end
   end # Freight
