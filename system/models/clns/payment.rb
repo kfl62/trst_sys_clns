504fb387a9b1a61a5e8f31f83a11397e951bd7b9
kfl62 <kfl62g@gmail.com>
2014-06-11 11:31:04 +0300

    models: `Clns::Payment` inherit from `Trst`, only overwrite

--- system/models/clns/payment.rb
+++ system/models/clns/payment.rb
@@ -1,23 +1,11 @@
 # encoding: utf-8
 module Clns
-  class Payment
-    include Mongoid::Document
-    include Mongoid::Timestamps
-    include Trst::ViewHelpers
-    include Trst::DateHelpers
+  class Payment < Trst::Payment
 
-    field :id_date,     type: Date,       default: -> {Date.today}
-    field :id_intern,   type: Boolean,    default: false
-    field :text,        type: String,     default: "Doc.nr."
-    field :val,         type: Float,      default: 0.00
-
-    belongs_to :doc_inv,      class_name: "Clns::Invoice",          inverse_of: :pyms
+    belongs_to :doc_inv,      class_name: "Clns::Invoice",              inverse_of: :pyms
 
     class << self
-      # @todo
-      def nonin(nin = true)
-        where(id_intern: !nin)
-      end
     end # Class methods
+
   end # Payment
 end # Clns
