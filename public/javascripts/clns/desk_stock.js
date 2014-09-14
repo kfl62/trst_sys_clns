e173eb7d7cd4f395853270c278335511d78504f5
kfl62 <kfl62g@gmail.com>
2014-09-14 14:00:43 +0300

    assets: js, `Clns.desk.stock` update, rewrite

--- public/javascripts/clns/desk_stock.js
+++ public/javascripts/clns/desk_stock.js
@@ -3,8 +3,35 @@
     $.extend(true, Clns, {
       desk: {
         stock: {
+          calculate: function() {},
+          inputs: function(inpts) {
+            inpts.each(function() {
+              var $ind, $input;
+              $input = $(this);
+              $ind = $input.data();
+            });
+          },
+          selects: function(slcts) {
+            slcts.each(function() {
+              var $sd, $select;
+              $select = $(this);
+              $sd = $select.data();
+            });
+          },
+          buttons: function(btns) {
+            btns.each(function() {
+              var $bd, $button, _ref;
+              $button = $(this);
+              $bd = $button.data();
+              if (Trst.desk.hdo.dialog === 'filter') {
+                if ((_ref = $bd.action) === 'create' || _ref === 'show' || _ref === 'edit' || _ref === 'delete') {
+                  $bd.r_path = 'sys/clns/stock/filter';
+                }
+              }
+            });
+          },
           init: function() {
-            $('.focus').focus();
+            Clns.desk.stock.buttons($('button'));
             return $log('Clns.desk.stock.init() OK...');
           }
         }
--- system/assets/javascripts/clns/desk_stock.coffee
+++ system/assets/javascripts/clns/desk_stock.coffee
@@ -2,9 +2,31 @@ define () ->
   $.extend true,Clns,
     desk:
       stock:
+        calculate: ()->
+          return
+        inputs: (inpts)->
+          inpts.each ()->
+            $input = $(@)
+            $ind = $input.data()
+            return
+          return
+        selects: (slcts)->
+          slcts.each ()->
+            $select = $(@)
+            $sd = $select.data()
+            return
+          return
+        buttons: (btns)->
+          btns.each ()->
+            $button = $(@)
+            $bd = $button.data()
+            if Trst.desk.hdo.dialog is 'filter'
+              if $bd.action in ['create','show','edit','delete']
+                $bd.r_path = 'sys/clns/stock/filter'
+            return
+          return
         init: ()->
-          $('.focus').focus()
-          # Clns.desk.stock.buttons($('button'))
+          Clns.desk.stock.buttons($('button'))
           # Clns.desk.stock.selects($('select.wstm'))
           # Clns.desk.stock.inputs($('input'))
           $log 'Clns.desk.stock.init() OK...'
